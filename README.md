501eccb95ed408a1f967a400c00ca44c95ccbb4d
HuynhTrung <nguyenhuynhtrung231224@gmail.com>
2026-07-02 12:31:30 +0700

    Bai tap dau tien cua C

---
+++ README.md
@@ -0,0 +1,2 @@
+"# NMLT-29-PS50529-HuynhTrung" 
+"# NMLT-29-PS50529-HuynhTrung" 
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Cuoi cung cung chay duoc !" << endl;
+    return 0;
+}
